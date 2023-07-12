c9ee8df528ca04ffdd447c7f7d360c2e999736a1
tawhidpro <towhid2302@gmail.com>
2023-07-12 23:49:58 +0600

    Favicon added and Particle  reduced

--- JS/app.js
+++ JS/app.js
@@ -64,7 +64,7 @@ particlesJS('particles-js',
         "enable": true,
         "distance": 150,
         "color": "#589CFF",
-        "opacity": 0.4,
+        "opacity": 0.3,
         "width": 1
       },
       "move": {
