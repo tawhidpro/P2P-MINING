6b438a839aa0c719a1625b8f2831372f50f3501c
tawhidpro <towhid2302@gmail.com>
2023-07-12 22:15:25 +0600

    The adjustments and issues in Skype have been fixed.

--- JS/custom.js
+++ JS/custom.js
@@ -13,3 +13,22 @@ $(this).prop('Counter',0).animate({
 });
 
 });  
+
+
+
+// scroll to top butotn 
+
+window.onscroll = function() {
+  var scrollButton = document.getElementById("scroll-to-top");
+  if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+};
+
+// Scroll to top when the button is clicked
+document.getElementById("scroll-to-top").onclick = function() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
